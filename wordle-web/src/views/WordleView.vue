48b70799e3298d5e4b5e970575ee5930ecdb77ce
VForev <85714161+VForev@users.noreply.github.com>
2023-05-30 15:02:51 -0700

    ...

--- wordle-web/src/views/WordleView.vue
+++ wordle-web/src/views/WordleView.vue
@@ -4,4 +4,12 @@
 
 <script setup lang="ts">
 import WordleGame from '@/components/WordleGame.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const date = computed(() => {
+  return route.query.date as string
+})
 </script>
